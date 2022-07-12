1b0e16260aa489516a080c7e7e7fc898fb5e79ff
WilliamZhu <allwefantasy@gmail.com>
2022-07-12 18:49:11 +0800

    udpate

---
+++ cmd/delete.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"mlsql.tech/allwefantasy/deploy/pkg/meta"
+)
+
+func delete(c *cli.Context) error {
+
+	k8sConfig := meta.BuildKubeConfig(c)
+	engineName := c.String("engine-name")
+
+	
+
+	return nil
+}
+
+func deleteFlags() *cli.Command {
+	cmd := &cli.Command{
+		Name:      "delete",
+		Usage:     "undeploy byzer engine from ",
+		ArgsUsage: "k8s",
+		Action:    delete,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "kube-config",
+				Required: true,
+				Usage:    "path of kube config file",
+			},
+			&cli.StringFlag{
+				Name:     "engine-name",
+				Required: true,
+				Usage:    "the engine-name we want to undeploy",
+			},
+		},
+	}
+
+	return cmd
+}
