6dcf14458567f53851360f13442c2e373f0fe0f8
WilliamZhu <allwefantasy@gmail.com>
2022-07-12 19:15:48 +0800

    update

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli/v2"
+	"mlsql.tech/allwefantasy/deploy/pkg/meta"
 )
 
 func delete(c *cli.Context) error {
 
-	//k8sConfig := meta.BuildKubeConfig(c)
-	//engineName := c.String("engine-name")
+	k8sConfig := meta.BuildKubeConfig(c)
+	engineName := c.String("engine-name")
 
+	metaConfig := meta.MetaConfig{
+		K8sConfig:    k8sConfig,
+		EngineConfig: meta.EngineConfig{Name: engineName},
+	}
 
+	executor := meta.CreateKubeExecutor(&metaConfig.K8sConfig)
+
+	logger.Info(fmt.Sprintf("delete ingress:%s", metaConfig.EngineConfig.Name))
+	executor.DeleteAny([]string{"ingress", metaConfig.EngineConfig.Name})
+
+	logger.Info(fmt.Sprintf("delete service:%s", metaConfig.EngineConfig.Name))
+	executor.DeleteAny([]string{"service", metaConfig.EngineConfig.Name})
+
+	logger.Info(fmt.Sprintf("delete deployment:%s", metaConfig.EngineConfig.Name))
+	executor.DeleteAny([]string{"deployment", metaConfig.EngineConfig.Name})
+
+	logger.Info(fmt.Sprintf("delete configmap:%s", fmt.Sprintf("%s-core-site-xml", metaConfig.EngineConfig.Name)))
+	executor.DeleteAny([]string{"configmap", fmt.Sprintf("%s-core-site-xml", metaConfig.EngineConfig.Name)})
 
 	return nil
 }
